6181d324a93e7bf84496f21afa672cb234642745
thots <SatyaDileepKumar.Thotakura@in.pega.com>
2019-06-23 22:38:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# flutter_kickstart
+
+Started coding with flutter.. of course I&#x27;m a noob.. sooner or later I&#x27;ll be no-more a noob..
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
+
+For help getting started with Flutter, view our 
+[online documentation](https://flutter.io/docs), which offers tutorials, 
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+// lets kickstart flutter learning.. removed all the auto gen code and started off from scratch
+import 'package:flutter/material.dart';
+
+/// just like in java we have a main function from where everything starts
+/// main defines what to run and where to start. runApp is the default function that allows us to attach a widget to the screen
+///By default all UI components are widgets in flutter.
+void main() {
+  runApp(
+    /// MaterialApp is a class in dart that is in app.dart file - its a widget that contains a lot of widgets
+    /// required for material design applications
+    MaterialApp(
+      /// scaffold is a screen that basically covers the entire main screen of the app
+        home: Scaffold(
+          /// appBar is a property that is used display at the top of scaffold
+          appBar: AppBar(
+            backgroundColor: Colors.amberAccent,
+          ),
+        )
+    ),
+  );
+}
